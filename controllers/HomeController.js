270f0eb2e44b0443db30e912d536455b17f48570
jose <lanza.josemaria@gmail.com>
2020-10-31 08:17:25 -0300

    feat-Comunication View-Controller

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,3 +1,13 @@
+'use strict';
+
 class HomeController {
-    
+    constructor() {
+        this.index();
+    }
+
+    index(saludo) {
+        console.log(saludo + ' mundo');
+    }
 }
+
+const homeController = new HomeController();
